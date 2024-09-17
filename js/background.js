1d1f094c71439dc1cef58ed9ce590c296f4f5f1e
Karis004 <2862309436@qq.com>
2024-09-17 10:05:16 +0800

    Refactor CSS styles and add omnibox functionality

--- js/background.js
+++ js/background.js
@@ -1,8 +1,26 @@
 chrome.omnibox.onInputEntered.addListener((text) => {
     chrome.storage.sync.get(["course_dict"], (data) => {
         const course_dict = data.course_dict;
-        console.log(course_dict);
-        console.log(course_dict[text]);
-        chrome.tabs.create({ url: course_dict[text]["url"] });
+        const course_code = text.toUpperCase();
+        for (const key in course_dict) {
+            if (key.includes(course_code)) {
+                chrome.tabs.create({ url: course_dict[key]["url"] });
+                return;
+            }
+        }
     });
-  });
+});
+
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+    chrome.storage.sync.get(["course_dict"], (data) => {
+        const course_dict = data.course_dict;
+        const suggestions = [];
+        const course_code = text.toUpperCase();
+        for (const key in course_dict) {
+            if (key.includes(course_code)) {
+                suggestions.push({ content: key, description: key+ ": " + course_dict[key]["detail"] });
+            }
+        }
+        suggest(suggestions);
+    });
+});
